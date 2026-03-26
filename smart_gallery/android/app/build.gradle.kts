f4ea0ef36dab1c2adfe777c819c6854fff240466
nsmofficial <sahilnavadiya8@gmail.com>
2026-03-26 11:58:33 +0530

    second commit

--- smart_gallery/android/app/build.gradle.kts
+++ smart_gallery/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.smartgallery.app"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,7 +24,7 @@ android {
         applicationId = "com.smartgallery.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
